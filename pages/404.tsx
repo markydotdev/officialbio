2e175132faad04d3346e0bd5344dcd0a057dd774
Marky R <99497246+devMarky@users.noreply.github.com>
2022-03-08 15:26:04 -0500

    Added custom 404

---
+++ pages/404.tsx
@@ -0,0 +1,28 @@
+import { BaseLayout } from '../components/BaseLayout';
+import Image from 'next/image';
+import { styled } from '../stitches.config';
+
+const Styled404 = styled('section', {
+  display: 'flex',
+  flexDirection: 'column',
+  justifyContent: 'center',
+  alignItems: 'center',
+  textAlign: 'center',
+});
+
+export default function Custom404() {
+  return (
+    <BaseLayout>
+      <Styled404>
+        <h2>404 - Page cannot be found</h2>
+        <Image
+          src='/ghost.png'
+          alt='cute 404 ghost'
+          width={200}
+          height={200}
+          layout={'fixed'}
+        />
+      </Styled404>
+    </BaseLayout>
+  );
+}
