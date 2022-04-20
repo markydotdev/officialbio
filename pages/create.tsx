fce1470fcf1bbc6f425a0c0688b0c0693b4687b4
Marky <99497246+tildemarky@users.noreply.github.com>
2022-04-20 18:32:24 -0400

    Add signed out page for new users

--- pages/create.tsx
+++ pages/create.tsx
@@ -1,9 +1,40 @@
-import React from 'react';
+import router from 'next/router';
+import React, { useContext } from 'react';
 
 import BuildingBlocks from '../components/BuildingBlocks';
+import Button from '../components/Button';
 import Layout from '../components/Layout';
+import { styled } from '../stitches.config';
+import { UserContext } from './_app';
+
+const CenteredPage = styled('section', {
+  height: '100%',
+  display: 'flex',
+  flexDirection: 'column',
+  justifyContent: 'center',
+  alignItems: 'center',
+});
 
 function create() {
+  const userId = useContext(UserContext);
+  if (userId === null) {
+    return (
+      <Layout>
+        <CenteredPage>
+          <h2>Sign in to start customizing your page</h2>
+          <Button
+            onClick={() => router.push('/sign_in')}
+            type={undefined}
+            disabled={undefined}
+            version={undefined}
+            loading={undefined}
+          >
+            Sign In
+          </Button>
+        </CenteredPage>
+      </Layout>
+    );
+  }
   return (
     <Layout>
       <BuildingBlocks />
