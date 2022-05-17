1d3cc9ed600c67d1b32e576e1dee5fe6868afb08
Marky R <99497246+tildemarky@users.noreply.github.com>
2022-05-16 21:27:33 -0400

    Change tab styling to accomodate mobile

--- components/PostForm/Tabs.tsx
+++ components/PostForm/Tabs.tsx
@@ -2,20 +2,33 @@ import { styled } from '../../stitches.config';
 
 const TabGroup = styled('div', {
   display: 'flex',
+  justifyContent: 'center',
+  flexWrap: 'wrap',
+  margin: '0.5rem',
 });
 const Tab = styled('button', {
-  border: 'none',
   fontSize: '$base',
+  fontWeight: 'bold',
   fontFamily: '$body',
   textAlign: 'center',
   minWidth: '6rem',
   textTransform: 'capitalize',
-  backgroundColor: '$gray4',
-  borderRadius: '$image $image 0 0',
   padding: '1rem',
+  margin: '0.1rem',
+  border: '2px solid transparent',
+  borderRadius: '99px',
+  transition: '$main',
+  '&:hover': {
+    backgroundColor: '$gray1',
+    transform: 'scale(1.01)',
+  },
+  '&:active': {
+    transform: 'scale(0.9)',
+  },
   variants: {
     active: {
       true: {
+        border: '2px solid $gray12',
         backgroundColor: '$gray1',
       },
     },
