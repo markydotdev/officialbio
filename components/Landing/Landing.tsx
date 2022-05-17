62e66e897aa5dd4fd1f710a31c5223f8f30a579c
Marky <99497246+tildemarky@users.noreply.github.com>
2022-05-17 17:12:20 -0400

    Add scroll snapping to landing page

--- components/Landing/Landing.tsx
+++ components/Landing/Landing.tsx
@@ -1,21 +1,29 @@
 import strings from '../../locales/en/strings';
 import Feature from './Feature';
-import Hero from './Hero';
 import Start from './Start';
+import { styled } from '../../stitches.config';
+
+const Box = styled('div', {
+  height: '45vh',
+  overflowY: 'scroll',
+  scrollSnapType: 'y mandatory',
+});
+const Snap = styled('div', {
+  height: '100%',
+  scrollSnapAlign: 'start',
+});
 
 function Landing() {
   return (
     <>
       <Start>{strings.start}</Start>
-      {/* <Hero
-        title={strings.landing.title}
-        subtitle={strings.landing.subtitle}
-        prompt={strings.landing.prompt}
-      /> */}
-
-      {strings.features.map((feature) => (
-        <Feature key={feature.id} {...feature} />
-      ))}
+      <Box>
+        {strings.features.map((feature) => (
+          <Snap key={feature.id}>
+            <Feature {...feature} />
+          </Snap>
+        ))}
+      </Box>
     </>
   );
 }
