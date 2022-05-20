26c13505bffbc46e06df38db860be9c66a897a27
Marky R <99497246+tildemarky@users.noreply.github.com>
2022-05-20 17:30:59 -0400

    Add more coherent styling for drag handles

--- components/ListOfLinks/ProfileLink.tsx
+++ components/ListOfLinks/ProfileLink.tsx
@@ -7,8 +7,8 @@ import { CSS } from '@dnd-kit/utilities';
 const LinkBox = styled('div', {
   display: 'flex',
   width: '100%',
-  flexDirection: 'column',
   justifyContent: 'space-between',
+  alignItems: 'center',
   marginTop: '1rem',
   borderRadius: '$image',
   padding: '1rem 0.75rem 0.5rem 0.75rem',
@@ -26,26 +26,39 @@ const LinkBox = styled('div', {
     margin: '0.5rem auto 0 auto',
   },
 });
-const LinkText = styled('a', {
-  margin: '0 0 0.5rem 0',
+const Text = styled('div', {
+  flex: 1,
+  paddingLeft: '1rem',
+});
+const LinkText = styled('p', {
+  margin: 0,
   fontSize: '$base',
   whiteSpace: 'pre-line',
   lineHeight: '1.1rem',
-});
-const ButtonGroup = styled('div', {
-  display: 'flex',
-  flexDirection: 'column',
-  '@md': {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
+  '& + &': {
+    paddingTop: '0.5rem',
   },
 });
-const TriggerIcon = styled('button', {
+const Icon = styled('button', {
   border: 'none',
   backgroundColor: 'transparent',
   transition: '$main',
   padding: 0,
   margin: 0,
+  cursor: 'grab',
+  variants: {
+    purpose: {
+      drag: {
+        cursor: 'grab',
+        '&:active': {
+          cursor: 'grabbing',
+        },
+      },
+      close: {
+        cursor: 'pointer',
+      },
+    },
+  },
   '&:hover': {
     transform: 'scale(1.1)',
   },
@@ -53,14 +66,35 @@ const TriggerIcon = styled('button', {
     transform: 'scale(0.9)',
   },
   '& > svg': {
-    width: 20,
-    height: 20,
+    width: 25,
+    height: 25,
   },
 });
 
+const DragHandle = ({ listeners, attributes }) => {
+  return (
+    <Icon purpose='drag' {...listeners} {...attributes}>
+      <svg
+        width='15'
+        height='15'
+        viewBox='0 0 15 15'
+        fill='none'
+        xmlns='http://www.w3.org/2000/svg'
+      >
+        <path
+          d='M5.5 4.625C6.12132 4.625 6.625 4.12132 6.625 3.5C6.625 2.87868 6.12132 2.375 5.5 2.375C4.87868 2.375 4.375 2.87868 4.375 3.5C4.375 4.12132 4.87868 4.625 5.5 4.625ZM9.5 4.625C10.1213 4.625 10.625 4.12132 10.625 3.5C10.625 2.87868 10.1213 2.375 9.5 2.375C8.87868 2.375 8.375 2.87868 8.375 3.5C8.375 4.12132 8.87868 4.625 9.5 4.625ZM10.625 7.5C10.625 8.12132 10.1213 8.625 9.5 8.625C8.87868 8.625 8.375 8.12132 8.375 7.5C8.375 6.87868 8.87868 6.375 9.5 6.375C10.1213 6.375 10.625 6.87868 10.625 7.5ZM5.5 8.625C6.12132 8.625 6.625 8.12132 6.625 7.5C6.625 6.87868 6.12132 6.375 5.5 6.375C4.87868 6.375 4.375 6.87868 4.375 7.5C4.375 8.12132 4.87868 8.625 5.5 8.625ZM10.625 11.5C10.625 12.1213 10.1213 12.625 9.5 12.625C8.87868 12.625 8.375 12.1213 8.375 11.5C8.375 10.8787 8.87868 10.375 9.5 10.375C10.1213 10.375 10.625 10.8787 10.625 11.5ZM5.5 12.625C6.12132 12.625 6.625 12.1213 6.625 11.5C6.625 10.8787 6.12132 10.375 5.5 10.375C4.87868 10.375 4.375 10.8787 4.375 11.5C4.375 12.1213 4.87868 12.625 5.5 12.625Z'
+          fill='currentColor'
+          fillRule='evenodd'
+          clipRule='evenodd'
+        ></path>
+      </svg>
+    </Icon>
+  );
+};
+
 const TriggerButton = () => {
   return (
-    <TriggerIcon type='button'>
+    <Icon purpose='close'>
       <svg
         width='15'
         height='15'
@@ -75,7 +109,7 @@ const TriggerButton = () => {
           clipRule='evenodd'
         ></path>
       </svg>
-    </TriggerIcon>
+    </Icon>
   );
 };
 
@@ -94,19 +128,18 @@ const ProfileLink = ({ id, linkId, display, link, removeLink }) => {
   // Have to use sortablecontext here...
   return (
     <LinkBox ref={setNodeRef} style={style}>
-      <LinkText href={link}>{display}</LinkText>
-      <ButtonGroup>
-        <button {...listeners} {...attributes}>
-          Drag handle
-        </button>
-        <Alert
-          title={strings.linkPage.title_remove}
-          description={strings.linkPage.description_remove}
-          action={handleRemove}
-          actionTitle={strings.linkPage.confirm_remove}
-          triggerButton={TriggerButton}
-        />
-      </ButtonGroup>
+      <DragHandle listeners={listeners} attributes={attributes} />
+      <Text>
+        <LinkText>{display}</LinkText>
+        <LinkText>{link}</LinkText>
+      </Text>
+      <Alert
+        title={strings.linkPage.title_remove}
+        description={strings.linkPage.description_remove}
+        action={handleRemove}
+        actionTitle={strings.linkPage.confirm_remove}
+        triggerButton={TriggerButton}
+      />
     </LinkBox>
   );
 };
