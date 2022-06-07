cc9bbd69e4ae56e9ba089a104d2f66e26c1d2170
Marky R <99497246+tildemarky@users.noreply.github.com>
2022-06-07 11:37:45 -0400

    add umami

--- components/Layout/Layout.tsx
+++ components/Layout/Layout.tsx
@@ -1,6 +1,7 @@
 import { styled } from '../../stitches.config';
 import NavMenu from '../NavMenu';
 import Footer from './Footer';
+import Script from 'next/script';
 
 const Container = styled('main', {
   marginTop: '1rem',
@@ -15,6 +16,11 @@ function Layout({ children }) {
       <NavMenu />
       <Container>{children}</Container>
       <Footer minimal={false} />
+
+      <Script
+        data-website-id='ccd8df66-a68e-4fe2-aa49-ad5bab85a0c7'
+        src='https://umamiocto.netlify.app/umami.js'
+      />
     </>
   );
 }
